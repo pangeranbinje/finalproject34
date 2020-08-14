68b07d5c284ab5608943ac9aa4978a426fec35e6
Fachru Rozi <pangeranbinje@gmail.com>
2020-08-14 22:29:50 +0700

    guest session

--- resources/views/partials/navBar.blade.php
+++ resources/views/partials/navBar.blade.php
@@ -149,7 +149,16 @@
 
     <!-- Nav Item - User Information -->
     <li class="nav-item dropdown no-arrow">
-
+    @guest
+      <li class="nav-item">
+          <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+      </li>
+    @if (Route::has('register'))
+      <li class="nav-item">
+          <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+      </li>
+    @endif
+    @else
       <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->user_name }}</span>
         <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
@@ -177,6 +186,7 @@
                                         @csrf
                                     </form>
       </div>
+      @endguest
     </li>
 
   </ul>
